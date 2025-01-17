c4b358a67cbf03fa546b8446f05c9bdbad511a53
Vivek <the9416legend@gmail.com>
2025-01-17 09:48:34 +0530

    [Vivek] added : new file with linked list basics

---
+++ 16-1-25/linked-list-creation.cpp
@@ -0,0 +1,28 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+class Node{
+public : 
+    int data;
+    Node* next ;
+
+    Node(int data){
+        this -> data = data;
+        this -> next = NULL; 
+    }    
+};
+
+void insertAtBegnning(int val){
+    Node* newNode = new Node(val);
+    newNode -> next = NULL;
+}
+
+int main()
+{
+    Node* n1 = new Node(10);
+    cout << n1 -> data << endl;
+    cout << n1 -> next << endl;
+
+    return 0;
+}
