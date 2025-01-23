c30037356fa0a790afc3e037f233685d281ca86c
Vivek <the9416legend@gmail.com>
2025-01-23 09:41:43 +0530

    [Vivek] updated : dynamic-array.cpp file

--- 21-1-25/dynamic-array.cpp
+++ 21-1-25/dynamic-array.cpp
@@ -1,49 +1,63 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
-class DynamicArray{
+class DynamicArray {
 public:
     int size;
-    int* arr;
     int cap;
+    int* arr;
 
-    DynamicArray(int size){
-        this -> size = size;
-        arr = new int[size];
+    DynamicArray(int initialSize) {
+        this->size = 0;
+        this->cap = initialSize;
+        arr = new int[cap];
     }
 
-    void resize(){
+    void resize() {
         cap = cap * 2;
         int* newArray = new int[cap];
-
         for (int i = 0; i < size; ++i) {
-            newArr[i] = arr[i];
+            newArray[i] = arr[i];
         }
-
-        delete arr[];
+        delete[] arr;
         arr = newArray;
-
     }
 
-    void add(int ele){
-        if(size == cap){
+    void add(int ele) {
+        if (size == cap) {
             resize();
         }
         arr[size++] = ele;
     }
 
-    void remove(){
-        --size;
+    void remove() {
+        if (size > 0) {
+            --size;
+        }
     }
 
-    int get(int ind){
+    int get(int ind) {
         return arr[ind];
     }
 
+   
 };
- 
-int main()
-{
- 
-return 0;
+
+int main() {
+    DynamicArray da(2);
+
+    da.add(10);
+    da.add(20);
+    da.add(30);
+
+    cout << "Array elements: ";
+    for (int i = 0; i < da.size; ++i) {
+        cout << da.get(i) << " ";
+    }
+    cout << endl;
+
+    da.remove();
+    cout << "After removal, size: " << da.size << endl;
+
+    return 0;
 }
